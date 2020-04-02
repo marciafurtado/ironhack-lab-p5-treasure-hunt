02a48a72ccdf4acc402e4f300c583dacf8b24789
Marcia Furtado <marciacristianef@gmail.com>
2020-04-02 23:05:59 +0200

    image error

--- src/game.js
+++ src/game.js
@@ -1,7 +1,36 @@
 class Game {
   drawGrid() {
+    
+    for(let i = 0; i < 11 ;i++){
+      line(0, i * SQUARE_SIDE, WIDTH, i * SQUARE_SIDE);
+      line(i * SQUARE_SIDE, 0,i * SQUARE_SIDE, HEIGHT);
+      // line(0,10,WIDTH, 10);
+    }
+  }
+}
+
+class Player {
+  constructor(col, row){
+    this.yCol = 0;
+    this.xRow = 0;
+  }
+
+  moveUp(){
+    this.yCol -= 1;
+  }
+  moveRight(){
+    this.xRow += 1;
+  }
+  moveLeft(){
+    this.xRow -= 1;
+  }
+  moveDown(){
+    this.yCol += 1;
+  }
+}
+
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
-  }
-}
+  
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 const game = new Game();
+let player1 = new Player();
+
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
@@ -6,5 +8,28 @@ function setup() {
 }
 
 function draw() {
+  clear();
+  // image(img, player1.xRow, player1.yCol);
   game.drawGrid();
+}
+
+function keyPressed(){
+  if(keyCode === 38){ 
+    player1.moveUp();
+  }
+  if(keyCode === 39){
+    player1.moveRight();
+  }
+  if(keyCode === 37){
+    player1.moveLeft();
+  }
+  if(keyCode === 40){
+    player1.moveDown();
+  }
+
+  console.log(player1.yCol, player1.xRow);
+}
+
+function preload(){
+  img = loadImage('./assets/character-down.png');
 }
